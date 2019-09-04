8a421bba2627388514a4c99636ff197cbb03dbb7
lianghao <lianghao@91bihu.com>
2019-09-04 17:58:34 +0800

    梁浩904晚间提交

--- Private_methods/The_customer_list_test/customer_list_test.py
+++ Private_methods/The_customer_list_test/customer_list_test.py
@@ -21,54 +21,145 @@ logger = Log()
 logbug = LogBug()
 
 
-class login:
-    def login_input(driver, user = elements["Denglu_tast_account"], pwd = elements["Denglu_test_password"], code = elements["Denglu_test_verifycode"]):
-        ActionChains(driver).double_click(driver.find_element_by_id(elements["Dengluzhanghao_id"])).perform()
-        time.sleep(1)
-        # driver.find_element_by_id(elements["Dengluzhanghao_id"]).send_keys(Keys.BACKSPACE)
-        # driver.find_element_by_id(elements["Dengluzhanghao_id"]).clear()
-        # time.sleep(1)
-        driver.find_element_by_id(elements["Dengluzhanghao_id"]).send_keys(user)
-        time.sleep(1)
-        ActionChains(driver).double_click(driver.find_element_by_id(elements["Denglumima_id"])).perform()
-        time.sleep(1)
-        # driver.find_element_by_id(elements["Denglumima_id"]).send_keys(Keys.BACKSPACE)
-        # driver.find_element_by_id(elements["Denglumima_id"]).clear()
-        # time.sleep(1)
-        driver.find_element_by_id(elements["Denglumima_id"]).send_keys(pwd)
-        time.sleep(1)
-        ActionChains(driver).double_click(driver.find_element_by_id(elements["Dengluyanzhengma_id"])).perform()
-        time.sleep(1)
-        # driver.find_element_by_id(elements["Dengluyanzhengma_id"]).clear()
-        # driver.find_element_by_id(elements["Dengluyanzhengma_id"]).send_keys(Keys.BACKSPACE)
-        # time.sleep(1)
-        driver.find_element_by_id(elements["Dengluyanzhengma_id"]).send_keys(code)
-        time.sleep(1)
-        driver.find_element_by_css_selector(elements["Denglu_anniu_css"]).click()
-        time.sleep(1)
+class customerlist:
+    def customer_list_tab_select(driver,select_tab):
+        if select_tab == '当期客户':
+            try:
+                driver.find_elements_by_css_selector(elements["kehuliebiao_tab_css"])[0].click()
+                logger.info(f"{select_tab} 打开成功")
+            except Exception:
+                logbug.debug(f"{select_tab} 打开失败")
 
-    def login_clear(driver):
-        driver.find_element_by_id(elements["Dengluzhanghao_id"]).clear()
-        time.sleep(1)
-        driver.find_element_by_id(elements["Denglumima_id"]).clear()
-        time.sleep(1)
-        driver.find_element_by_id(elements["Dengluyanzhengma_id"]).clear()
-        time.sleep(1)
+        elif select_tab == '首访客户':
+            try:
+                driver.find_elements_by_css_selector(elements["kehuliebiao_tab_css"])[1].click()
+                logger.info(f"{select_tab} 打开成功")
+            except Exception:
+                logbug.debug(f"{select_tab} 打开失败")
 
-    def login_check_errormsg(driver, hint):
-        test = driver.find_element_by_class_name(elements["Denglu_errormessage_class"]).text
-        if test == hint:
-            logger.info("传值为:{}结果为:{}".format(hint, test))
-            return True
+        elif select_tab == '未回访':
+            try:
+                driver.find_elements_by_css_selector(elements["kehuliebiao_tab_css"])[2].click()
+                logger.info(f"{select_tab} 打开成功")
+            except Exception:
+                logbug.debug(f"{select_tab} 打开失败")
+
+        elif select_tab == '计划回访':
+            try:
+                driver.find_elements_by_css_selector(elements["kehuliebiao_tab_css"])[3].click()
+                logger.info(f"{select_tab} 打开成功")
+            except Exception:
+                logbug.debug(f"{select_tab} 打开失败")
+
+        elif select_tab == '今日到店':
+            try:
+                driver.find_elements_by_css_selector(elements["kehuliebiao_tab_css"])[4].click()
+                logger.info(f"{select_tab} 打开成功")
+            except Exception:
+                logbug.debug(f"{select_tab} 打开失败")
+
+        elif select_tab == '预约进店':
+            try:
+                driver.find_elements_by_css_selector(elements["kehuliebiao_tab_css"])[5].click()
+                logger.info(f"{select_tab} 打开成功")
+            except Exception:
+                logbug.debug(f"{select_tab} 打开失败")
+
+        elif select_tab == '逾期客户':
+            try:
+                driver.find_elements_by_css_selector(elements["kehuliebiao_tab_css"])[6].click()
+                logger.info(f"{select_tab} 打开成功")
+            except Exception:
+                logbug.debug(f"{select_tab} 打开失败")
+
+        elif select_tab == '全部客户':
+            try:
+                driver.find_elements_by_css_selector(elements["kehuliebiao_tab_css"])[7].click()
+                logger.info(f"{select_tab} 打开成功")
+            except Exception:
+                logbug.debug(f"{select_tab} 打开失败")
         else:
-            logbug.debug("传值为:{}结果为:{}".format(hint, test))
-            return False
+            pass
 
-    def login_check_success(driver):
-        if driver.find_element_by_css_selector(
-                elements["Denglu_success_dropdown_css"]).text == elements["Denglu_success_dropdown_check_text"]:
-            logger.info("{}登录成功".format(elements["Denglu_tast_account"]))
-            return True
-        else:
-            logbug.debug("{}登录失败".format(elements["Denglu_tast_account"]))
-            return False
+
+    def search_chepai(driver,input_chepai = elements["test_licenseno"]):
+        try:
+            driver.find_element_by_css_selector(elements["kehuliebiao_sousuo1_dropdown_css"]).click()
+            time.sleep(1)
+            driver.find_elements_by_css_selector(elements["kehuliebiao_sousuo1_dropdown_selection_css"])[0].click()
+            time.sleep(1)
+            driver.find_element_by_css_selector(elements["kehuliebiao_sousuo1_textbox_css"]).send_keys(input_chepai)
+            time.sleep(1)
+            driver.find_element_by_css_selector(elements["kehuliebiao_sousuo_button_css"]).click()
+            time.sleep(1)
+            logger.info(f'{input_chepai} 搜索成功')
+        except Exception:
+            logbug.debug(f'{input_chepai} 搜索失败')
+
+    def search_chejia(driver,input_chejia = elements["test_vin"]):
+        try:
+            driver.find_element_by_css_selector(elements["kehuliebiao_sousuo1_dropdown_css"]).click()
+            time.sleep(1)
+            driver.find_elements_by_css_selector(elements["kehuliebiao_sousuo1_dropdown_selection_css"])[0].click()
+            time.sleep(1)
+            driver.find_element_by_css_selector(elements["kehuliebiao_sousuo1_textbox_css"]).send_keys(input_chejia)
+            time.sleep(1)
+            driver.find_element_by_css_selector(elements["kehuliebiao_sousuo_button_css"]).click()
+            time.sleep(1)
+            logger.info(f'{input_chejia} 搜索成功')
+        except Exception:
+            logbug.debug(f'{input_chejia} 搜索失败')
+
+
+    def search_chezhuxingming(driver,input_chezhuxingming = elements["test_chezhuxingming"]):
+        try:
+            driver.find_element_by_css_selector(elements["kehuliebiao_sousuo1_dropdown_css"]).click()
+            time.sleep(1)
+            driver.find_elements_by_css_selector(elements["kehuliebiao_sousuo1_dropdown_selection_css"])[1].click()
+            time.sleep(1)
+            driver.find_element_by_css_selector(elements["kehuliebiao_sousuo1_textbox_css"]).send_keys(input_chezhuxingming)
+            time.sleep(1)
+            driver.find_element_by_css_selector(elements["kehuliebiao_sousuo_button_css"]).click()
+            time.sleep(1)
+            logger.info(f'{input_chezhuxingming} 搜索成功')
+        except Exception:
+            logbug.debug(f'{input_chezhuxingming} 搜索失败')
+
+    def search_kehumingcheng(driver,input_kehumingcheng = elements["test_kehumingcheng"]):
+        try:
+            driver.find_element_by_css_selector(elements["kehuliebiao_sousuo1_dropdown_css"]).click()
+            time.sleep(1)
+            driver.find_elements_by_css_selector(elements["kehuliebiao_sousuo1_dropdown_selection_css"])[2].click()
+            time.sleep(1)
+            driver.find_element_by_css_selector(elements["kehuliebiao_sousuo1_textbox_css"]).send_keys(input_kehumingcheng)
+            time.sleep(1)
+            driver.find_element_by_css_selector(elements["kehuliebiao_sousuo_button_css"]).click()
+            time.sleep(1)
+            logger.info(f'{input_kehumingcheng} 搜索成功')
+        except Exception:
+            logbug.debug(f'{input_kehumingcheng} 搜索失败')
+
+    def search_kehudianhua(driver, input_kehudianhua=elements["test_kehudianhua1"]):
+        try:
+            driver.find_element_by_css_selector(elements["kehuliebiao_sousuo1_dropdown_css"]).click()
+            time.sleep(1)
+            driver.find_elements_by_css_selector(elements["kehuliebiao_sousuo1_dropdown_selection_css"])[3].click()
+            time.sleep(1)
+            driver.find_element_by_css_selector(elements["kehuliebiao_sousuo1_textbox_css"]).send_keys(
+                input_kehudianhua)
+            time.sleep(1)
+            driver.find_element_by_css_selector(elements["kehuliebiao_sousuo_button_css"]).click()
+            time.sleep(1)
+            logger.info(f'{input_kehudianhua} 搜索成功')
+        except Exception:
+            logbug.debug(f'{input_kehudianhua} 搜索失败')
+
+    def search_daoqishijian(driver, input_daoqishijian=elements["test_kehudianhua1"]):
+        try:
+            driver.find_element_by_css_selector(elements["kehuliebiao_sousuo2_daoqishijian_css"]).click()
+            time.sleep(1)
+            logger.info(f'{input_daoqishijian} 搜索成功')
+        except Exception:
+            logbug.debug(f'{input_daoqishijian} 搜索失败')
+
+
--- Public_tast_case/test_customer_list.py
+++ Public_tast_case/test_customer_list.py
@@ -8,6 +8,7 @@ from utils.Logbug import LogBug
 from utils.browser_engine import BrowserEngin
 from Private_methods.Private_login_test.login_test import login
 from Public_methods.New_offer_menu import menu
+from Private_methods.The_customer_list_test.customer_list_test import customerlist
 logger = Log()
 logbug = LogBug()
 #  读取配置文件
@@ -18,9 +19,17 @@ class Customer_Lict(unittest.TestCase):
         browser = BrowserEngin()
         self.driver = browser.open_browser()
 
-    def test_customer_list_yanzheng1(self):
-       menu1 = menu()
-       menu1.Select_Menu_case(self.driver,'新增报价')
+    def test_customer_list_tab(self):
+        login.login_input(self.driver)
+        time.sleep(5)
+        menu1 = menu()
+        menu1.Select_Menu_case(self.driver , '客户列表')
+        time.sleep(2)
+        customerlist.search_daoqishijian(self.driver , 'daoqi')
+        time.sleep(1)
+
+
+
 
     @classmethod
     def tearDown(self):
